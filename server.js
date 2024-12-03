22238f1d1785ee8fccdd93ea79b29e2ed77c949a
Sebastian <sebastian.degerman@consid.se>
2024-12-03 21:49:41 +0100

    fix missing package

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
-const WebSocket = require('ws');
+const WebSocketServer = require('websocket').server;
+const http = require('http');
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = http.createServer((req, res) => {
+  res.writeHead(404);
+  res.end();
+});
+
+server.listen(8080, () => {
+  console.log('Signaling server is running on ws://localhost:8080');
+});
+
+const wsServer = new WebSocketServer({ httpServer: server });
 const peers = new Set();
 
-server.on('connection', socket => {
-  peers.add(socket);
+wsServer.on('request', request => {
+  const connection = request.accept(null, request.origin);
+  peers.add(connection);
   console.log('New peer connected!');
 
-  socket.on('message', message => {
-    for (let peer of peers) {
-      if (peer !== socket) {
-        peer.send(message);
+  connection.on('message', message => {
+    if (message.type === 'utf8') {
+      const text = message.utf8Data;
+      // Broadcast the message to all other peers
+      for (let peer of peers) {
+        if (peer !== connection) {
+          peer.sendUTF(text);
+        }
       }
     }
   });
 
-  socket.on('close', () => {
-    peers.delete(socket);
+  connection.on('close', () => {
+    peers.delete(connection);
     console.log('Peer disconnected!');
   });
 });
-
-console.log('Signaling server is running on ws://localhost:8080');
